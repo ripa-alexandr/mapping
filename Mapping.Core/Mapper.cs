d1c8b0fa05a5cb0218a586a4d5722b14488eafd7
Alexander Ripa <alexander.ripa@muranosoft.com>
2016-09-23 15:33:55 +0300

    init

---
+++ Mapping.Core/Mapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapping.Core
+{
+	public static class Mapper
+	{
+	}
+}
