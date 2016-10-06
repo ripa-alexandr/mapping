83d71d5ee2f7f2c5603ab8aab347130232a8c0ae
Alexander Ripa <alexander.ripa@muranosoft.com>
2016-10-06 13:16:05 +0300

    use Singletone

--- Mapping.Core/Mapper.cs
+++ Mapping.Core/Mapper.cs
@@ -4,34 +4,59 @@ using Mapping.Core.Extensions;
 
 namespace Mapping.Core
 {
-	public static class Mapper
+	public sealed class Mapper
 	{
-		private static readonly Dictionary<string, Map> mappings;
+		#region Singleton
 
-		static Mapper ()
+		private static readonly Object syncObj = new Object();
+		private static Mapper instance;
+		private readonly Dictionary<string, Map> mappings;
+
+		public static Mapper Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					lock (syncObj)
+					{
+						if (instance == null)
+						{
+							instance = new Mapper();
+						}
+					}
+				}
+
+				return instance;
+			}
+		}
+
+		private Mapper ()
 		{
 			mappings = new Dictionary<string, Map>();
 		}
+
+		#endregion
 
 		public static void CreateMap<TSource, TDestination> ()
 		{
 			string key = string.Concat(typeof(TSource).FullName, typeof(TDestination).FullName);
 
-			mappings.Add(key, new Map(typeof(TSource), typeof(TDestination)));
+			Instance.mappings.Add(key, new Map(typeof(TSource), typeof(TDestination)));
 		}
 
 		public static TDestination Map<TSource, TDestination> (TSource source) where TDestination : new() 
 		{
 			string key = string.Concat(typeof(TSource).FullName, typeof(TDestination).FullName);
 
-			if (!mappings.ContainsKey(key))
+			if (!Instance.mappings.ContainsKey(key))
 			{
 				throw new InvalidOperationException("Mapping does not exist. Call Initialize with appropriate configuration.");
 			}
 
 			TDestination destination = new TDestination();
 
-			foreach (var mapItem in mappings[key].MapItems)
+			foreach (var mapItem in Instance.mappings[key].MapItems)
 			{
 				var sourceValue = mapItem.Source.GetValue(source);
 
